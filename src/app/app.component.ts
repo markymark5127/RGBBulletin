26aca84d1f06a6cb67373c9cf947fc8591bbf823
Mark Mayne <mayne_mark@bah.com>
2019-09-23 15:06:36 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,178 @@
-import { Component } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
+
+export interface Nav {
+  cat: string;
+  subCat?: Nav[];
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'rgbbulletin';
+export class AppComponent implements OnDestroy {
+  title = 'RGB Bulletin';
+  mobileQuery: MediaQueryList;
+
+  // List of consoles applies to all
+  consoles: Nav[] = [
+    {
+      cat: 'Atari',
+      subCat: [
+        {
+          cat: 'Home Consoles',
+          subCat: [
+            { cat: 'Atari 2600' },
+            { cat: 'Atari 5200' },
+            { cat: 'Atari 7800' },
+            { cat: 'Jaguar'}
+          ]
+        },
+        {
+          cat: 'Handheld Consoles',
+          subCat: [
+            { cat: 'Lynx' }
+          ]
+        },
+      ]
+    },
+    {
+      cat: 'Microsoft',
+      subCat: [
+        { cat: 'Xbox' },
+        { cat: 'Xbox 360' },
+        { cat: 'Xbox One' },
+        { cat: 'Project Scarlett' }
+      ]
+    },
+    {
+      cat: 'NEC'
+    },
+    {
+      cat: 'Nintendo',
+      subCat: [
+        {
+          cat: 'Home Consoles',
+          subCat: [
+            { cat: 'NES' },
+            { cat: 'Super Nintendo'},
+            { cat: 'Nintendo 64' },
+            { cat: 'Gamecube' },
+            { cat: 'Wii' },
+            { cat: 'Wii U' },
+            { cat: 'Switch' }
+          ]
+        },
+        {
+          cat: 'Handheld Consoles',
+          subCat: [
+            { cat: 'Gameboy' },
+            { cat: 'Gameboy Color' },
+            { cat: 'Virtual Boy' },
+            { cat: 'Gameboy Advanced' },
+            { cat: 'Nintendo DS' },
+            { cat: 'Nintendo 3DS' },
+            { cat: 'Switch' }
+          ]
+        }
+      ]
+    },
+    {
+      cat: 'Phillips'
+    },
+    {
+      cat: 'Sega',
+      subCat: [
+        {
+          cat: 'Home Consoles',
+          subCat: [
+            { cat: 'SG 1000' },
+            { cat: 'Master System' },
+            { cat: 'Genesis/MegaDrive' },
+            { cat: 'Saturn' },
+            { cat: 'Dreamcast' }
+          ]
+        },
+        {
+          cat: 'Handheld Consoles',
+          subCat: [
+            { cat: 'Game Gear' },
+            { cat: 'Nomad' }
+          ]
+        }
+      ]
+    },
+    {
+      cat: 'Sony',
+      subCat: [
+        {
+          cat: 'Home Consoles',
+          subCat: [
+            { cat: 'Playstation' },
+            { cat: 'Playstation 2' },
+            { cat: 'Playstation 3' },
+            { cat: 'Playstation 4' }
+          ]
+        },
+        {
+          cat: 'Handheld Consoles',
+          subCat: [
+            { cat: 'PSP' },
+            { cat: 'Vita' }
+          ]
+        }
+      ]
+
+    }
+
+  ];
+
+  nav: Nav[] = [
+    {
+      cat: 'Console Info',
+      subCat: this.consoles
+    },
+    {
+      cat: 'Unfinished Games',
+      subCat: this.consoles
+    },
+    {
+      cat: 'Display',
+      subCat: [
+        {
+          cat: 'Connection',
+          subCat: [
+            { cat: 'RF' },
+            { cat: 'Composite' },
+            { cat: 'S-Video' },
+            { cat: 'YPbPr' },
+            { cat: 'RGB SCART' },
+            { cat: 'VGA' },
+            { cat: 'HDMI' },
+            { cat: 'Display Port'}
+          ]
+        },
+        { cat: 'Switches' },
+        { cat: 'Upscalers' },
+
+      ]
+    }
+  ];
+
+  // tslint:disable-next-line: variable-name
+  private _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    // tslint:disable-next-line: deprecation
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngOnDestroy(): void {
+    // tslint:disable-next-line: deprecation
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatListModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
